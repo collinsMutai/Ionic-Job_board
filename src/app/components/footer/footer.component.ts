aaf0e1a4ca01741b614dd7c2f9dd5cddc545ef9e
Collins Mutai <collinsfrontend@gmail.com>
2025-04-25 13:34:18 +0300

    ui

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule],
+})
+export class FooterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,13 +5,14 @@ import { IonicModule } from '@ionic/angular';
 import { NavComponent } from '../components/nav/nav.component';
 import { HeroComponent } from '../components/hero/hero.component';
 import { JobcardsComponent } from "../components/jobcards/jobcards.component";
+import { FooterComponent } from "../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, NavComponent, HeroComponent, JobcardsComponent],
+  imports: [IonicModule, NavComponent, HeroComponent, JobcardsComponent, FooterComponent],
 })
 export class HomePage {
   constructor() {}
